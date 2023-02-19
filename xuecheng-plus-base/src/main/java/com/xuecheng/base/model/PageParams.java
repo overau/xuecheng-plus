645ce9456968783385e5cfc6e6b167179bec2465
HeJin <deciphers@163.com>
2023-02-19 15:19:23 +0800

    feat(课程查询): 配置课程查询swagger接口文档
    1.配置课程查询swagger接口文档
    2.新增LocalDateTimeConfig处理时间字段序列化格式

--- xuecheng-plus-base/src/main/java/com/xuecheng/base/model/PageParams.java
+++ xuecheng-plus-base/src/main/java/com/xuecheng/base/model/PageParams.java
@@ -1,5 +1,6 @@
 package com.xuecheng.base.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
@@ -23,11 +24,13 @@ public class PageParams {
     /**
      * 当前页码
      */
+    @ApiModelProperty("当前页码")
     private Long pageNo = DEFAULT_PAGE_CURRENT;
 
     /**
      * 每页记录数默认值
      */
+    @ApiModelProperty("每页记录数")
     private Long pageSize = DEFAULT_PAGE_SIZE;
 
     public PageParams(){
