97683e9563b4875504c333f8bb9ed28efb7952da
HeJin <deciphers@163.com>
2023-02-20 16:23:22 +0800

    feat(课程分类查询): mybatis-plus生成新增课程分类相关代码
    mybatis-plus生成器生成新增课程分类相关代码

---
+++ xuecheng-plus-generator/src/main/java/com/xuecheng/content/service/CourseCategoryService.java
@@ -0,0 +1,16 @@
+package com.xuecheng.content.service;
+
+import com.xuecheng.content.model.po.CourseCategory;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 课程分类 服务类
+ * </p>
+ *
+ * @author HeJin
+ * @since 2023-02-20
+ */
+public interface CourseCategoryService extends IService<CourseCategory> {
+
+}
