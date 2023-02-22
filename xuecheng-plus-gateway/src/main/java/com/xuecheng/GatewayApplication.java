12dbfd1052311bbc99306ad212c464ac31c0c8d1
HeJin <deciphers@163.com>
2023-02-22 23:11:20 +0800

    feat(Gateway网关): 搭建Gateway网关
    使用nacos注册中心和配置中心，搭建Gateway网关

---
+++ xuecheng-plus-gateway/src/main/java/com/xuecheng/GatewayApplication.java
@@ -0,0 +1,18 @@
+package com.xuecheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author HeJin
+ * @version 1.0
+ * @since 2023/02/22 22:40
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
