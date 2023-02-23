274572f2a0f35363c7286d98c78d8c2cf32c3957
HeJin <deciphers@163.com>
2023-02-23 20:42:25 +0800

    feat(上传文件): 抽取service层文件上传代码
    抽取service层文件上传代码，进行优化

--- xuecheng-plus-media/xuecheng-plus-media-service/src/main/java/com/xuecheng/media/service/impl/MediaFileServiceImpl.java
+++ xuecheng-plus-media/xuecheng-plus-media-service/src/main/java/com/xuecheng/media/service/impl/MediaFileServiceImpl.java
@@ -2,6 +2,9 @@ package com.xuecheng.media.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.j256.simplemagic.ContentInfo;
+import com.j256.simplemagic.ContentInfoUtil;
+import com.xuecheng.base.exception.XueChengPlusException;
 import com.xuecheng.base.model.PageParams;
 import com.xuecheng.base.model.PageResult;
 import com.xuecheng.media.mapper.MediaFilesMapper;
@@ -15,12 +18,15 @@ import io.minio.PutObjectArgs;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.util.Date;
@@ -89,51 +95,78 @@ public class MediaFileServiceImpl implements MediaFileService {
             // objectName为空，使用文件的md5值为objectName
             objectName = fileMd5 + filename.substring(filename.lastIndexOf('.'));
         }
-
         objectName = folder + objectName;
+
+        // 1.上传文件到minio
+        addMediaFilesToMinIo(bytes, bucketFiles, objectName);
+        // 2.保存到数据库
+        MediaFiles mediaFiles = addMediaFilesToDb(companyId, uploadFileParamsDto, bucketFiles, objectName, fileMd5);
+        // 3.准备返回数据
+        UploadFileResultDto uploadFileResultDto = new UploadFileResultDto();
+        BeanUtils.copyProperties(mediaFiles, uploadFileResultDto);
+        return uploadFileResultDto;
+    }
+
+    /**
+     * 文件信息保存到数据库
+     * @param companyId 机构id
+     * @param uploadFileParamsDto 文件信息
+     * @param objectName 对象名称
+     * @param fileId 文件id
+     * @return 媒资信息
+     */
+    private MediaFiles addMediaFilesToDb(Long companyId, UploadFileParamsDto uploadFileParamsDto, String bucket, String objectName, String fileId) {
+        MediaFiles mediaFiles = mediaFilesMapper.selectById(fileId);
+        if (null == mediaFiles){
+            mediaFiles = new MediaFiles();
+            BeanUtils.copyProperties(uploadFileParamsDto, mediaFiles);
+            mediaFiles.setId(fileId);
+            mediaFiles.setFileId(fileId);
+            mediaFiles.setCompanyId(companyId);
+            mediaFiles.setBucket(bucket);
+            mediaFiles.setFilePath(objectName);
+            mediaFiles.setUrl("/" + bucket + "/" + objectName);
+            mediaFiles.setCreateDate(LocalDateTime.now());
+            mediaFiles.setStatus("1");
+            mediaFiles.setAuditStatus("002003");
+            // 插入文件表
+            mediaFilesMapper.insert(mediaFiles);
+        }
+        return mediaFiles;
+    }
+
+    /**
+     * 将文件上传到分布式文件系统
+     * @param bytes 文件字节数组
+     * @param bucket 桶名称
+     * @param objectName 对象名称
+     */
+    private void addMediaFilesToMinIo(byte[] bytes, String bucket, String objectName) {
         try (ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes)) {
-            String contentType = uploadFileParamsDto.getContentType();
-            //  long objectSize(对象大小), long partSize(-1表示5M)
+            String contentType = MediaType.APPLICATION_OCTET_STREAM_VALUE;
+            if (objectName.contains(".")){
+                String extension = objectName.substring(objectName.lastIndexOf('.'));
+                ContentInfo extensionMatch = ContentInfoUtil.findExtensionMatch(extension);
+                if (extensionMatch != null){
+                    contentType = extensionMatch.getMimeType();
+                }
+            }
             PutObjectArgs putObjectArgs = PutObjectArgs.builder()
-                    .bucket(bucketFiles)
+                    .bucket(bucket)
                     .object(objectName)
                     .stream(byteArrayInputStream, byteArrayInputStream.available(), -1)
                     .contentType(contentType)
                     .build();
-            // 1.上传到minion
             minioClient.putObject(putObjectArgs);
-            // 2.保存到数据库
-            MediaFiles mediaFiles = mediaFilesMapper.selectById(fileMd5);
-            if (null == mediaFiles){
-                mediaFiles = new MediaFiles();
-                BeanUtils.copyProperties(uploadFileParamsDto, mediaFiles);
-                mediaFiles.setId(fileMd5);
-                mediaFiles.setFileId(fileMd5);
-                mediaFiles.setCompanyId(companyId);
-                mediaFiles.setFilename(filename);
-                mediaFiles.setBucket(bucketFiles);
-                mediaFiles.setFilePath(objectName);
-                mediaFiles.setUrl("/" + bucketFiles + "/" + objectName);
-                mediaFiles.setCreateDate(LocalDateTime.now());
-                mediaFiles.setStatus("1");
-                mediaFiles.setAuditStatus("002003");
-
-                // 插入文件表
-                mediaFilesMapper.insert(mediaFiles);
-            }
-            // 3.准备返回数据
-            UploadFileResultDto uploadFileResultDto = new UploadFileResultDto();
-            BeanUtils.copyProperties(mediaFiles, uploadFileResultDto);
-
-            return uploadFileResultDto;
         } catch (Exception e) {
-            log.debug("上传文件失败: {}", e.getMessage());
+            e.printStackTrace();
+            log.error("上传文件到文件系统出错: {}", e.getMessage());
+            throw new XueChengPlusException("上传文件出错!");
         }
-
-        return null;
     }
 
-    /**
+
+        /**
      * 根据日期拼接目录
      * @param date 日期
      * @param year 是否包含年
