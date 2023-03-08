df11ce529d1445e06a67f7317ad66bb9b4b3ba45
HeJin <deciphers@163.com>
2023-03-08 09:19:57 +0800

    feat(认证授权): 创建认证服务工程
    创建认证服务工程

---
+++ xuecheng-plus-auth/src/main/java/com/xuecheng/AuthApplication.java
@@ -0,0 +1,16 @@
+package com.xuecheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Administrator
+ */
+@SpringBootApplication
+public class AuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+
+}
---
+++ xuecheng-plus-auth/src/main/java/com/xuecheng/ucenter/model/dto/AuthParamsDto.java
@@ -0,0 +1,51 @@
+package com.xuecheng.ucenter.model.dto;
+
+import lombok.Data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Mr.M
+ * @version 1.0
+ * @description 认证用户请求参数
+ * @date 2022/9/29 10:56
+ */
+@Data
+public class AuthParamsDto {
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 手机号
+     */
+    private String cellphone;
+
+    /**
+     * 验证码
+     */
+    private String checkcode;
+
+    /**
+     * 验证码key
+     */
+    private String checkcodekey;
+
+    /**
+     * 认证的类型   password:用户名密码模式类型    sms:短信模式类型
+     */
+    private String authType;
+
+    /**
+     * //附加数据，作为扩展，不同认证类型可拥有不同的附加数据。如认证类型为短信时包含smsKey : sms:3d21042d054548b08477142bbca95cfa; 所有情况下都包含clientId
+     */
+    private Map<String, Object> payload = new HashMap<>();
+}
