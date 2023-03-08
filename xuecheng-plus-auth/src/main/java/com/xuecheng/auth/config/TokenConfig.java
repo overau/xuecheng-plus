124b9043407f3ad362f0959b96dbd17391433455
HeJin <deciphers@163.com>
2023-03-08 10:20:48 +0800

    feat(认证授权): 测试oauth2生成JWT令牌
    测试oauth2生成JWT令牌

--- xuecheng-plus-auth/src/main/java/com/xuecheng/auth/config/TokenConfig.java
+++ xuecheng-plus-auth/src/main/java/com/xuecheng/auth/config/TokenConfig.java
@@ -4,10 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 import javax.annotation.Resource;
+import java.util.Collections;
 
 /**
  * @author Administrator
@@ -16,14 +19,27 @@ import javax.annotation.Resource;
 @Configuration
 public class TokenConfig {
 
+    private static final String SIGNING_KEY = "abcdefg";
+
     @Resource
     TokenStore tokenStore;
 
+    @Resource
+    private JwtAccessTokenConverter accessTokenConverter;
+
     @Bean
     public TokenStore tokenStore() {
-        //使用内存存储令牌（普通令牌）
-        return new InMemoryTokenStore();
+        //使用内存存储令牌（普通令牌）: new InMemoryTokenStore()
+        return new JwtTokenStore(accessTokenConverter());
     }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey(SIGNING_KEY);
+        return converter;
+    }
+
 
     @Bean(name = "authorizationServerTokenServicesCustom")
     public AuthorizationServerTokenServices tokenService() {
@@ -32,6 +48,9 @@ public class TokenConfig {
         service.setSupportRefreshToken(true);
         //令牌存储策略
         service.setTokenStore(tokenStore);
+        TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
+        tokenEnhancerChain.setTokenEnhancers(Collections.singletonList(accessTokenConverter));
+        service.setTokenEnhancer(tokenEnhancerChain);
         // 令牌默认有效期2小时
         service.setAccessTokenValiditySeconds(7200);
         // 刷新令牌默认有效期3天
